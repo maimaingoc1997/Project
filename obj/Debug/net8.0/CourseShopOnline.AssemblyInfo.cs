e2cec2be924466054e6c7f6eb70771e01b7eb2bd
Mai do <mai.do@evizi.com>
2025-04-08 09:19:51 +0700

    update

--- obj/Debug/net8.0/CourseShopOnline.AssemblyInfo.cs
+++ obj/Debug/net8.0/CourseShopOnline.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseShopOnline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea16d7fe22a49f464f58cd6d0e27ac8ab29f5c35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc53ac6e611523909294b5fbc4b88ce1b3acf0ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseShopOnline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseShopOnline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
